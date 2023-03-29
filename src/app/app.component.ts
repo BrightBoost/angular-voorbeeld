e4faea79d3c43544b3fccea92619d96b85be8bf5
brightboost <info@brightboost.nl>
2023-03-29 10:46:00 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'Hoi guys';
+  msg = 'Blabla';
+  nrPuppy = '1';
+
+  getPuppy() {
+    return 'assets/images/puppy' + this.nrPuppy + '.jpeg';
+  }
 }
